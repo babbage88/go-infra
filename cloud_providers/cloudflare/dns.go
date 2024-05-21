5f462341041cf809845edd7b527da4dcb3c21692
Justin Trahan <justin@trahan.dev>
2024-05-21 00:40:33 +0000

    Added update record

--- cloud_providers/cloudflare/dns.go
+++ cloud_providers/cloudflare/dns.go
@@ -2,29 +2,59 @@ package cloudflaredns
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
+	"strings"
 )
 
 type CloudflareDnsZone struct {
-	BaseUrl     string
-	ZoneId      string
-	RecordId    string
-	CfToken     string
-	DnsRecordId string
+	BaseUrl       string        `json:"baseUrl"`
+	ZoneId        string        `json:"zoneId"`
+	CfToken       string        `json:"cfToken"`
+	RecordRequest *DnsRecordReq `json:"recordRequest"`
+	SecretsPath   string        `json:"secretsPath"`
 }
 
-func GetCurrentRecords(czone *CloudflareDnsZone) {
-	//url := "https://api.cloudflare.com/client/v4/zones/[dns_zone_id]/dns_records"
+type DnsRecordReq struct {
+	Content     string `json:"content"`
+	Name        string `json:"name"`
+	Proxied     bool   `json:"proxied"`
+	Type        string `json:"type"`
+	Comment     string `json:"comment"`
+	Ttl         int16  `json:"ttl"`
+	DnsRecordId string `json:"dnsRecordId"`
+}
+
+func buildRequestUrl(czone *CloudflareDnsZone) string {
 	var bUrl bytes.Buffer
 
 	bUrl.WriteString(czone.BaseUrl)
 	bUrl.WriteString(czone.ZoneId)
 	bUrl.WriteString("/dns_records")
+	if czone.RecordRequest.DnsRecordId != "" {
+		bUrl.WriteString("/" + czone.RecordRequest.DnsRecordId)
+	}
 
 	url := bUrl.String()
+
+	return url
+}
+
+func createPayload(record *DnsRecordReq) (io.Reader, error) {
+	data, err := json.Marshal(record)
+	if err != nil {
+		return nil, err
+	}
+	return strings.NewReader(string(data)), nil
+}
+
+func GetCurrentRecords(czone *CloudflareDnsZone) *http.Response {
+	//url := "https://api.cloudflare.com/client/v4/zones/[dns_zone_id]/dns_records"
+
+	url := buildRequestUrl(czone)
 
 	req, _ := http.NewRequest("GET", url, nil)
 
@@ -40,20 +70,16 @@ func GetCurrentRecords(czone *CloudflareDnsZone) {
 	slog.Debug(fmt.Sprint(res))
 	slog.Debug(fmt.Sprint(string(body)))
 	slog.Info("Retrieving Current DNS Records")
-	fmt.Println(string(body))
+	//fmt.Println(string(body))
+
+	return res
 
 }
 
-func GetDnsRecordDetails(czone *CloudflareDnsZone) {
+func GetDnsRecordDetails(czone *CloudflareDnsZone) *http.Response {
 	//url := "https://api.cloudflare.com/client/v4/zones/zone_id/dns_records/dns_record_id"
 
-	var bUrl bytes.Buffer
-
-	bUrl.WriteString(czone.BaseUrl)
-	bUrl.WriteString(czone.ZoneId)
-	bUrl.WriteString("/dns_records" + czone.DnsRecordId)
-
-	url := bUrl.String()
+	url := buildRequestUrl(czone)
 
 	req, _ := http.NewRequest("GET", url, nil)
 
@@ -63,8 +89,44 @@ func GetDnsRecordDetails(czone *CloudflareDnsZone) {
 	res, _ := http.DefaultClient.Do(req)
 
 	defer res.Body.Close()
-	body, _ := io.ReadAll(res.Body)
 
-	fmt.Println(res)
-	fmt.Println(string(body))
+	return res
+}
+
+func CreateDnsRecord(czone *CloudflareDnsZone) *http.Response {
+	url := buildRequestUrl(czone)
+
+	payload, err := createPayload(czone.RecordRequest)
+	if err != nil {
+		fmt.Println("Error creating payload:", err)
+	}
+	req, _ := http.NewRequest("POST", url, payload)
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+czone.CfToken)
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+
+	return res
+}
+
+func UpdateDnsRecord(czone *CloudflareDnsZone) *http.Response {
+	url := buildRequestUrl(czone)
+
+	payload, err := createPayload(czone.RecordRequest)
+	if err != nil {
+		fmt.Println("Error creating payload:", err)
+	}
+	req, _ := http.NewRequest("PATCH", url, payload)
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+czone.CfToken)
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+
+	return res
 }
--- main.go
+++ main.go
@@ -22,14 +22,24 @@ func main() {
 
 	clog.Info("Starting Infra tasks", slog.String("Action", "Retrieving DNS Records"))
 
+	dnsreq := &cloudflaredns.DnsRecordReq{
+		Content:     "10.0.0.32",
+		Name:        "testgo",
+		Proxied:     false,
+		Type:        "A",
+		Comment:     "Testing Golang",
+		Ttl:         3600,
+		DnsRecordId: "your_record_id_here",
+	}
+
 	// Create a CloudflareDnsZone object with hardcoded values
 	czone := &cloudflaredns.CloudflareDnsZone{
-		BaseUrl:  "https://api.cloudflare.com/client/v4/zones/",
-		ZoneId:   "your_zone_id",
-		RecordId: "your_record_id_here",
-		CfToken:  "your_token_here",
+		BaseUrl:       "https://api.cloudflare.com/client/v4/zones/",
+		ZoneId:        "you_stuff_here",
+		CfToken:       "your_token_here",
+		RecordRequest: dnsreq,
 	}
 
 	// Call the GetCurrentRecords function
-	cloudflaredns.GetCurrentRecords(czone)
+	cloudflaredns.CreateDnsRecord(czone)
 }
