36c505320643772d429b9af99c912c0cd68ccf78
Justin Trahan <justin@trahan.dev>
2024-05-21 00:58:40 +0000

    Reading from env vars

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log/slog"
+	"os"
 
 	cloudflaredns "git.trahan.dev/go-infra/cloud_providers/cloudflare"
 	customlogger "git.trahan.dev/go-infra/utils"
@@ -21,6 +23,10 @@ func main() {
 	}()
 
 	clog.Info("Starting Infra tasks", slog.String("Action", "Retrieving DNS Records"))
+	var api_key string = os.Getenv("CLOUFLARE_DNS_KEY")
+	var cf_zone_ID string = os.Getenv("CF_ZONE_ID")
+	fmt.Println("API Auth Token: " + api_key)
+	fmt.Println("Zone ID: " + cf_zone_ID)
 
 	dnsreq := &cloudflaredns.DnsRecordReq{
 		Content:     "10.0.0.32",
@@ -35,11 +41,11 @@ func main() {
 	// Create a CloudflareDnsZone object with hardcoded values
 	czone := &cloudflaredns.CloudflareDnsZone{
 		BaseUrl:       "https://api.cloudflare.com/client/v4/zones/",
-		ZoneId:        "you_stuff_here",
-		CfToken:       "your_token_here",
+		ZoneId:        cf_zone_ID,
+		CfToken:       api_key,
 		RecordRequest: dnsreq,
 	}
-
+	fmt.Println(fmt.Sprint(czone))
 	// Call the GetCurrentRecords function
-	cloudflaredns.CreateDnsRecord(czone)
+	//cloudflaredns.CreateDnsRecord(czone)
 }
