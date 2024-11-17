2c49f7ef4c4f3284f4834e30803fb8cc093feba0
Justin Trahan <justin.m.trahan@outlook.com>
2024-11-17 00:58:45 +0000

    cleanup main

--- main.go
+++ main.go
@@ -1,61 +1,22 @@
 package main
 
 import (
-	"database/sql"
 	"flag"
-	"fmt"
 	"log/slog"
 
 	"github.com/babbage88/go-infra/auth/hashing"
 	infra_db "github.com/babbage88/go-infra/database/infra_db"
-	db_models "github.com/babbage88/go-infra/database/models"
-	env_helper "github.com/babbage88/go-infra/utils/env_helper"
 	test "github.com/babbage88/go-infra/utils/test"
 	"github.com/babbage88/go-infra/webapi/api_server"
 	_ "github.com/pdrum/swagger-automation/docs"
 )
 
-func createTestUserInstance(username string, password string, email string, role string) db_models.User {
-	hashedpw, err := hashing.HashPassword(password)
-	if err != nil {
-		slog.Error("Error hashing password", slog.String("Error", err.Error()))
-	}
-
-	testuser := db_models.User{
-		Username: username,
-		Password: hashedpw,
-		Email:    email,
-		Role:     role,
-	}
-
-	return testuser
-}
-
-func initializeDbConn(envars *env_helper.EnvVars) *sql.DB {
-	db_name := envars.GetVarMapValue("DB_NAME")
-	db_pw := envars.GetVarMapValue("DB_PW")
-	db_user := envars.GetVarMapValue("DB_USER")
-	db_host := envars.GetVarMapValue("DB_HOST")
-	db_port, err := envars.ParseEnvVarInt32("DB_PORT")
-	if err != nil {
-		fmt.Errorf("Error Parsing DB_PORT from .env file", err)
-	}
-	dbConn := infra_db.NewDatabaseConnection(infra_db.WithDbHost(db_host),
-		infra_db.WithDbPassword(db_pw),
-		infra_db.WithDbUser(db_user),
-		infra_db.WithDbPort(db_port),
-		infra_db.WithDbName(db_name))
-
-	db, _ := infra_db.InitializeDbConnection(dbConn)
-
-	return db
-}
-
 func main() {
 
 	srvport := flag.String("srvadr", ":8993", "Address and port that http server will listed on. :8993 is default")
-	hostEnvironment := flag.String("envfile", ".env", "Path to .env file to load Environment Variables.")
+	envFilePath := flag.String("envfile", ".env", "Path to .env file to load Environment Variables.")
 	username := flag.String("username", "jtrahan", "Username to create")
+	emailuser := flag.String("email", "testdev@trahan.dev", "email for new username")
 	version := flag.Bool("version", false, "Show the current version.")
 	testfuncs := flag.Bool("test", false, "run test module")
 	flag.Parse()
@@ -65,11 +26,8 @@ func main() {
 		return
 	}
 
-	env_helper.LoadEnvFile(*hostEnvironment)
-	envars := env_helper.NewDotEnvSource(env_helper.WithDotEnvFileName(*hostEnvironment))
-
-	fmt.Printf("EnVars file name: %s\n", envars.DotFileName)
-	envars.ParseEnvVariables()
+	envars := initEnvironment(*envFilePath)
+	connPool := initPgConnPool()
 
 	if *testfuncs {
 		login_pw := envars.GetVarMapValue("DEV_APP_TEST_PW")
@@ -77,7 +35,7 @@ func main() {
 		if err != nil {
 			slog.Info("Error hashing password.")
 		}
-		test.TestCreateUserQuery(*username, hashed_pw)
+		test.TestCreateNewUser(connPool, *username, hashed_pw, *emailuser, "admin")
 	}
 
 	db := initializeDbConn(envars)
--- utils/test/test.go
+++ utils/test/test.go
@@ -1,85 +1,16 @@
 package test
 
 import (
-	"context"
 	"log"
-	"log/slog"
-	"os"
-	"time"
 
-	"github.com/babbage88/go-infra/database/infra_db_pg"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/babbage88/go-infra/database/db_access"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func pgxPoolConfig() *pgxpool.Config {
-	const defaultMaxConns = int32(4)
-	const defaultMinConns = int32(0)
-	const defaultMaxConnLifetime = time.Hour
-	const defaultMaxConnIdleTime = time.Minute * 30
-	const defaultHealthCheckPeriod = time.Minute
-	const defaultConnectTimeout = time.Second * 5
-	connString := os.Getenv("DATABASE_URL")
-
-	dbConfig, err := pgxpool.ParseConfig(connString)
+func TestCreateNewUser(connPool *pgxpool.Pool, username string, hashed_pw string, email string, role string) (db_access.UserDao, error) {
+	newuser, err := db_access.CreateUserQuery(connPool, username, hashed_pw, email, role)
 	if err != nil {
-		slog.Error("Failed to create a config, error: ", "Error", err)
+		log.Fatalf("Error creating user %s", err)
 	}
-
-	dbConfig.MaxConns = defaultMaxConns
-	dbConfig.MinConns = defaultMinConns
-	dbConfig.MaxConnLifetime = defaultMaxConnLifetime
-	dbConfig.MaxConnIdleTime = defaultMaxConnIdleTime
-	dbConfig.HealthCheckPeriod = defaultHealthCheckPeriod
-	dbConfig.ConnConfig.ConnectTimeout = defaultConnectTimeout
-	dbConfig.BeforeAcquire = func(ctx context.Context, c *pgx.Conn) bool {
-		slog.Info("Before acquiring the connection pool to the database!!")
-		return true
-	}
-
-	dbConfig.AfterRelease = func(c *pgx.Conn) bool {
-		slog.Info("After releasing the connection pool to the database!!")
-		return true
-	}
-
-	dbConfig.BeforeClose = func(c *pgx.Conn) {
-		log.Println("Closed the connection pool to the database!!")
-	}
-
-	return dbConfig
-
-}
-
-func TestCreateUserQuery(username string, hashed_pw string) (infra_db_pg.User, error) {
-	// Create database connection
-	connPool, err := pgxpool.NewWithConfig(context.Background(), pgxPoolConfig())
-	if err != nil {
-		slog.Error("Error while creating connection to the database!!", "Error", err)
-	}
-
-	connection, err := connPool.Acquire(context.Background())
-	if err != nil {
-		slog.Error("Error while acquiring connection from the database pool!!", "Error", err)
-	}
-	defer connection.Release()
-
-	err = connection.Ping(context.Background())
-	if err != nil {
-		slog.Error("Could not ping database")
-	}
-
-	slog.Info("Connected to the database!!", "Database", os.Getenv("DATABASE_URL"))
-
-	// Set up parameters for the new user
-	params := infra_db_pg.CreateUserParams{
-		Username: pgtype.Text{String: "johndoe", Valid: true},
-		Password: pgtype.Text{String: "password123", Valid: true},
-		Email:    pgtype.Text{String: "johndoe@example.com", Valid: true},
-		Role:     pgtype.Text{String: "user", Valid: true},
-	}
-
-	queries := infra_db_pg.New(connPool)
-	newUser, err := queries.CreateUser(context.Background(), params)
-	return newUser, err
+	return newuser, err
 }
