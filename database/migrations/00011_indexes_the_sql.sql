3f7642b349df31c6b5930b1fb3a8ea584fbde119
Justin Trahan <justin.m.trahan@outlook.com>
2024-11-19 01:35:01 +0000

    Updated users indexes

--- database/migrations/00011_indexes_the_sql.sql
+++ database/migrations/00011_indexes_the_sql.sql
@@ -2,10 +2,12 @@
 -- +goose StatementBegin
 CREATE INDEX IF NOT EXISTS users_idx_enabled ON users("enabled");
 CREATE INDEX IF NOT EXISTS users_idx_role ON users("role");
+CREATE INDEX IF NOT EXISTS users_idx_created ON users(created_at);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DROP INDEX IF EXISTS users_idx_enabled;
 DROP INDEX IF EXISTS users_idx_role;
+DROP INDEX IF EXISTS users_idx_created;
 -- +goose StatementEnd
