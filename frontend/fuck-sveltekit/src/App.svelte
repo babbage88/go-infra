ff15d48e2e0b245f2a3bf010dc605d2e026d4d5e
Justin Trahan <justin.m.trahan@outlook.com>
2024-11-26 18:49:33 +0000

    added svelte [not kit] .with tailwind

--- frontend/fuck-sveltekit/src/App.svelte
+++ frontend/fuck-sveltekit/src/App.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import "./app.css";
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
--- frontend/fuck-sveltekit/vite.config.ts
+++ frontend/fuck-sveltekit/vite.config.ts
@@ -1,18 +1,12 @@
-import path from "path";
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  resolve: {
-    alias: {
-      $lib: path.resolve("./src/lib"),
-    },
-  },
   server: {
     host: true,
-    port: 3002,
-    origin: "http://0.0.0.0:3002"
+    port: 3001,
+    origin: "http://10.0.0.32:3001"
   }
 })
