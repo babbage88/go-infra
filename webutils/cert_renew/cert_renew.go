a89d82c2ddf0124ee2ccf08c06ba49d9c27b2898
Justin Trahan <justin@trahan.dev>
2026-04-24 19:12:19 -0500

    re-added go swagger annotations

--- webutils/cert_renew/cert_renew.go
+++ webutils/cert_renew/cert_renew.go
@@ -1,10 +1,38 @@
 package cert_renew
 
-import corecert "github.com/babbage88/infra-core/cert_renew"
+import "time"
 
-type CertificateDataRenewResponse = corecert.CertificateDataRenewResponse
-type CertificateData = corecert.CertificateData
-type CertDnsRenewReqWrapper = corecert.CertDnsRenewReqWrapper
-type CertDnsRenewReq = corecert.CertDnsRenewReq
-type Renewal = corecert.Renewal
-type CertRenewReq = corecert.CertRenewReq
+// swagger:response CertificateDataRenewResponse
+type CertificateDataRenewResponse struct {
+	Body CertificateData
+}
+
+// swagger:model CertificateData
+type CertificateData struct {
+	DomainNames   []string `json:"domainName"`
+	CertPEM       string   `json:"cert_pem"`
+	ChainPEM      string   `json:"chain_pem"`
+	Fullchain     string   `json:"fullchain_pem"`
+	PrivKey       string   `json:"priv_key"`
+	ZipDir        string   `json:"zipDir"`
+	S3DownloadUrl string   `json:"s3DownloadUrl"`
+}
+
+// Login Request takes in the certificate renewal payload.
+// swagger:parameters Renew
+type CertDnsRenewReqWrapper struct {
+	// in:body
+	Body CertDnsRenewReq `json:"body"`
+}
+
+// swagger:model CertDnsRenewReq
+type CertDnsRenewReq struct {
+	DomainNames          []string      `json:"domainName"`
+	AcmeEmail            string        `json:"acmeEmail"`
+	AcmeUrl              string        `json:"acmeUrl"`
+	ZipDir               string        `json:"zipDir"`
+	PushS3               bool          `json:"pushS3"`
+	Token                string        `json:"token"`
+	RecursiveNameServers []string      `json:"recurseServers"`
+	Timeout              time.Duration `json:"timeout"`
+}
--- webutils/cert_renew/handlers.go
+++ webutils/cert_renew/handlers.go
@@ -6,6 +6,16 @@ import (
 	corecert "github.com/babbage88/infra-core/cert_renew"
 )
 
+// swagger:route POST /renew Certificates Renew
+// Request/Renew ssl certificate via cloudflare letsencrypt. Uses DNS Challenge
+// responses:
+//  200: CertificateDataRenewResponse
+//  400: description:Bad Request
+//  401: description:Unauthorized
+//  500: description:Insernal Server Error
+// produces:
+// - application/json
+// - application/zip
 func Renewcert_renew() http.HandlerFunc {
 	return corecert.Renewcert_renew()
 }
